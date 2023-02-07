095d0ce44c1ca4f2b7303688359ac8e7ce3e8cc6
Jean Paul <jeanpaulvillalobos@gmail.com>
2023-02-06 20:57:45 -0500

    Agregamos divs ocultos con javascript y el archivo y se configuran divs

--- js/main-ani.js
+++ js/main-ani.js
@@ -1,9 +1,22 @@
+/*
+ * Ocultar los divs de las referencias familiares 
+ */
 let familia001 = document.getElementById('family_ref001');
 familia001.style.display = "none";
 let familia002 = document.getElementById('family_ref002');
 familia002.style.display = "none";
 let familia003 = document.getElementById('family_ref003');
 familia003.style.display = "none";
+
+/*
+ Ocultar los divs de las referencias personales
+*/
+let person001 = document.getElementById('personal_ref001');
+person001.style.display = "none";
+
+
+
+// Funcion que oculta y muestra
 
 function myFunction(nombre) {
     var x = document.getElementById(nombre);
@@ -13,7 +26,9 @@ function myFunction(nombre) {
         x.style.display = "none";
     }
 }
-
+/** 
+  Acccion de los botones de las referencias familiares
+**/
 let fm_ref_button01 = document.getElementById('button-fam-ref01');
 fm_ref_button01.addEventListener('click', ()=>{
     myFunction('family_ref001');
@@ -26,3 +41,7 @@ let fm_ref_button03 = document.getElementById('button-fam-ref03');
 fm_ref_button03.addEventListener('click', ()=>{
     myFunction('family_ref003');
 })
+
+/*
+Accion de los botones de las referencias personales
+*/
