4518773ed6c19ae89d733a415b0e1d71c7e111d6
Jean Paul <jeanpaulvillalobos@gmail.com>
2023-02-07 01:20:35 -0500

    Posible finalizacion del proyecto ojo posible ya que se agrega javascript

--- js/main-ani.js
+++ js/main-ani.js
@@ -7,13 +7,20 @@ let familia002 = document.getElementById('family_ref002');
 familia002.style.display = "none";
 let familia003 = document.getElementById('family_ref003');
 familia003.style.display = "none";
+let familia004 = document.getElementById('family_ref004');
+familia004.style.display = "none";
 
 /*
  Ocultar los divs de las referencias personales
 */
 let person001 = document.getElementById('personal_ref001');
 person001.style.display = "none";
-
+let person002 = document.getElementById('personal_ref002');
+person002.style.display = "none";
+let person003 = document.getElementById('personal_ref003');
+person003.style.display = "none";
+let person004 = document.getElementById('personal_ref004');
+person004.style.display = "none";
 
 
 // Funcion que oculta y muestra
@@ -41,7 +48,27 @@ let fm_ref_button03 = document.getElementById('button-fam-ref03');
 fm_ref_button03.addEventListener('click', ()=>{
     myFunction('family_ref003');
 })
-
+let fm_ref_button04 = document.getElementById('button-fam-ref04');
+fm_ref_button04.addEventListener('click', ()=>{
+    myFunction('family_ref004');
+})
 /*
 Accion de los botones de las referencias personales
 */
+
+let pr_ref_button01 = document.getElementById('button-per-ref01');
+pr_ref_button01.addEventListener('click', ()=>{
+    myFunction('personal_ref001');
+})
+let pr_ref_button02 = document.getElementById('button-per-ref02');
+pr_ref_button02.addEventListener('click', ()=>{
+    myFunction('personal_ref002');
+})
+let pr_ref_button03 = document.getElementById('button-per-ref03');
+pr_ref_button03.addEventListener('click', ()=>{
+    myFunction('personal_ref003');
+})
+let pr_ref_button04 = document.getElementById('button-per-ref04');
+pr_ref_button04.addEventListener('click', ()=>{
+    myFunction('personal_ref004');
+})
